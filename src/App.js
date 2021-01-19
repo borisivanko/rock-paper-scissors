b38caa58b1f04a1fe10577d89d957d961c1a278f
borisivanko <borisivanko004@gmail.com>
2021-01-19 13:21:09 +0100

    created logic and design

--- src/App.js
+++ src/App.js
@@ -5,18 +5,77 @@ import './App.css';
 function App() {
 	const options = ['Rock', 'Paper', 'Scissors'];
 	const [playerOption, setPlayerOption] = useState();
+	const [computerOption, setComputerOption] = useState();
+	const [playerScore, setPlayerScore] = useState(0);
+	const [computerScore, setComputerScore] = useState(0);
+	const [message, setMessage] = useState();
+
+	const selectOption = (player) => {
+		setPlayerOption(player);
+		setComputerOption(options[Math.floor(Math.random() * options.length)]);
+
+		if (playerOption === 'Rock') {
+			if (computerOption === 'Scissors') {
+				setMessage('You won!');
+				setPlayerScore(playerScore + 1);
+			} else if (computerOption === 'Paper') {
+				setMessage('You lost!');
+				setComputerScore(computerScore + 1);
+			} else {
+				setMessage('Tie!');
+			}
+		}
+
+		if (playerOption === 'Paper') {
+			if (computerOption === 'Rock') {
+				setMessage('You won!');
+				setPlayerScore(playerScore + 1);
+			} else if (computerOption === 'Scissors') {
+				setMessage('You lost!');
+				setComputerScore(computerScore + 1);
+			} else {
+				setMessage('Tie!');
+			}
+		}
+
+		if (playerOption === 'Scissors') {
+			if (computerOption === 'Paper') {
+				setMessage('You won!');
+				setPlayerScore(playerScore + 1);
+			} else if (computerOption === 'Rock') {
+				setMessage('You lost!');
+				setComputerScore(computerScore + 1);
+			} else {
+				setMessage('Tie!');
+			}
+		}
+	};
+
 	return (
 		<>
 			<main>
-				<div>
-					<FaHandRock />
-				</div>
-				<div>
-					<FaHandPaper />
-				</div>
-				<div>
-					<FaHandScissors />
-				</div>
+				<section className='wrapper'>
+					<h2>Player: {playerScore}</h2>
+					<h2>Computer: {computerScore}</h2>
+				</section>
+				<section className='wrapper'>
+					<div className='options' onClick={() => selectOption('Rock')}>
+						<FaHandRock className='option' />
+					</div>
+					<div className='options' onClick={() => selectOption('Paper')}>
+						<FaHandPaper className='option' />
+					</div>
+					<div className='options' onClick={() => selectOption('Scissors')}>
+						<FaHandScissors className='option' />
+					</div>
+				</section>
+				<section className='wrapper'>
+					{message ? (
+						<h2>{message}</h2>
+					) : (
+						<h2>Select one of the above to start game!</h2>
+					)}
+				</section>
 			</main>
 		</>
 	);
