8184ad5ed79a0e27a8481a4b0a459a56a6c83794
borisivanko <borisivanko004@gmail.com>
2021-01-19 12:33:51 +0100

    added icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React, { useState } from 'react';
+import { FaHandRock, FaHandPaper, FaHandScissors } from 'react-icons/fa';
 import './App.css';
 
 function App() {
-	return <></>;
+	const options = ['Rock', 'Paper', 'Scissors'];
+	const [playerOption, setPlayerOption] = useState();
+	return (
+		<>
+			<main>
+				<div>
+					<FaHandRock />
+				</div>
+				<div>
+					<FaHandPaper />
+				</div>
+				<div>
+					<FaHandScissors />
+				</div>
+			</main>
+		</>
+	);
 }
 
 export default App;
